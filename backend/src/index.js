2970725fba2d7c2340961f7733b41481e09541d4
Yowj <eugenecoderx@gmail.com>
2025-06-08 04:40:39 +0800

    Edited env

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,30 +2,28 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {connectDB} from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
-
+const PORT = process.env.NODE_ENV === "development" ? 5001 : 5002;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : process.env.FRONTEND,
     credentials: true, // allow frontend to send cookies
   })
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/chat", chatRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.send("Your server is running");
 });
-
-const PORT = process.env.PORT || (process.env.NODE_ENV === "development" ? 5001 : 5000);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);

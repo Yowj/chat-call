1c43b117b30f943f633811a9f50d48158f8384b6
Yowj <eugenecoderx@gmail.com>
2025-06-08 04:51:56 +0800

    missing api2

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,6 +1,8 @@
 import { StreamChat } from "stream-chat";
 import dotenv from "dotenv"
 
+dotenv.config();
+
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
 
